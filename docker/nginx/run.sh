30968dcaf6faa1344282247d7757b2c48e02e03b
48763 <future.starshine@gmail.com>
2020-10-05 00:58:33 +0800

    Update run.sh

--- docker/nginx/run.sh
+++ docker/nginx/run.sh
@@ -4,6 +4,6 @@ docker stop $DOCKER_CONTAINER_HASH
 docker rm $DOCKER_CONTAINER_HASH
 
 docker run --name nginx \
-        -v $(pwd)/../init.sh:/usr/share/nginx/html \
+        -v $(pwd)/../../init.sh:/usr/share/nginx/html/index/html \
         -p 80:80 \
-        -d nginx:1.0.1
+        -d 48763/nginx:1.12.2
