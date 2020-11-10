ad7e0b47b602ec12e0986889401b206d91099d3b
48763 <future.starshine@gmail.com>
2020-11-10 11:35:00 +0800

    Update entrypoint.sh

--- docker/nagios/entrypoint.sh
+++ docker/nagios/entrypoint.sh
@@ -1,6 +1,8 @@
-service apache2 start
+if [ ! $(grep /usr/local/nagios/etc/project $@) ]; then
+    line=$(( 2 + $(grep -n "cfg_dir$" $@ | cut -d : -f1) ))
+    sed -i "${line} a cfg_dir=/usr/local/nagios/etc/project" $@
+fi
 
-line=$(( 2 + $(grep -n "cfg_dir$" $@ | cut -d : -f1) ))
-sed -i "${line} a cfg_dir=/usr/local/nagios/etc/project" $@
+service apache2 start
 
 exec "./bin/nagios" "$@"
